28d048c0ae31077be7242a28832d391401bb655c
merab7 <merabtodua7@gmail.com>
2024-07-29 16:28:21 +0400

    initial

--- gecom/settings.py
+++ gecom/settings.py
@@ -15,7 +15,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=True)
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'kharikhatrans-production.up.railway.app']
 
@@ -88,7 +88,7 @@ DATABASES = {
 }
 
 
-POSTGRES_LOCALLY=False
+POSTGRES_LOCALLY=True
 if env.bool('PRODUCTION') or POSTGRES_LOCALLY:
     DATABASES['default']= dj_database_url.parse(env('DATABASE_URL'))
 
